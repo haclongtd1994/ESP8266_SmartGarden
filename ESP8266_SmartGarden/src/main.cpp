da2fb219fd7481b2c243cd5d387d4b232b985945
haclongtd1994 <haclongtd1994@gmail.com>
2023-11-06 09:24:50 +0700

    11/5/2023 - The status of the development

--- ESP8266_SmartGarden/src/main.cpp
+++ ESP8266_SmartGarden/src/main.cpp
@@ -32,6 +32,7 @@
 #define AIO_SERVERPORT  1883                   // use 8883 for SSL
 #define AIO_USERNAME    "haclongtd1994"
 #define AIO_KEY         "aio_VTME684H9f8mST4NzMpJudb9CfWp"
+#define MQTT_CONN_KEEPALIVE 300
 
 /************************* Global Time Setup *********************************/
 const long utcOffsetInSeconds = 25200;
@@ -53,6 +54,7 @@ Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO
 // Setup a feed called 'photocell' for publishing.
 // Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
 Adafruit_MQTT_Publish Status_Stream = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/status-stream");
+Adafruit_MQTT_Publish MultiTextReport = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/multitextreport");
 
 // Setup a feed called 'onoff' for subscribing to changes.
 Adafruit_MQTT_Subscribe onoffbutton_man = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/onoffbutton_man");
@@ -69,21 +71,41 @@ Adafruit_MQTT_Subscribe adjuststopvalue_m = Adafruit_MQTT_Subscribe(&mqtt, AIO_U
 // for some reason (only affects ESP8266, likely an arduino-builder bug).
 void MQTT_connect();
 void Publish_Value_x(uint32_t x);
+void Publish_Log();
 
+/*************************** Variable ************************************/
+volatile uint32_t Automate_Flag = 0;
+volatile uint32_t Manual_Set = 0;
+String AdjStartValueString_h = "9";
+String AdjStartValueString_m = "30";
+String AdjStopValueString_h = "9";
+String AdjStopValueString_m = "40";
+volatile uint32_t x=0;
+
+char buffer [5000] = {0};
+int cx;
+int ind = 0;
+
+/*************************** Setup Code ************************************/
 void setup() {
   Serial.begin(115200);
-  delay(10);
+  delay(20);
+  Serial.println();
 
-  Serial.println(F("Adafruit MQTT demo"));
+  Serial.println(F("Adafruit MQTT Smart Garden"));
+  cx = snprintf ( buffer + ind, 5000, "Adafruit MQTT Smart Garden\n");
+
   pinMode(0, OUTPUT);
   pinMode(2, OUTPUT);
-      digitalWrite(0,HIGH);
-      digitalWrite(2,HIGH);
+  digitalWrite(0,HIGH);
+  digitalWrite(2,HIGH);
 
   // Connect to WiFi access point.
-  Serial.println(); Serial.println();
+  Serial.println();
   Serial.print("Connecting to ");
   Serial.println(WLAN_SSID);
+  ind += cx;
+  cx = snprintf ( buffer + ind, 5000-ind, "Connecting to %s\n", WLAN_SSID);
 
   WiFi.begin(WLAN_SSID, WLAN_PASS);
   while (WiFi.status() != WL_CONNECTED) {
@@ -93,7 +115,10 @@ void setup() {
   Serial.println();
 
   Serial.println("WiFi connected");
-  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  ind += cx;
+  cx = snprintf ( buffer + ind, 5000-ind, "WiFi connected\nIP address: %s\n", WLAN_SSID);
 
   // Start timeclient
   timeClient.begin();
@@ -107,29 +132,31 @@ void setup() {
   mqtt.subscribe(&adjuststopvalue_m);
 }
 
-volatile uint32_t Automate_Flag = 0;
-volatile uint32_t Manual_Set = 0;
-String AdjStartValueString_h = "9";
-String AdjStartValueString_m = "30";
-String AdjStopValueString_h = "9";
-String AdjStopValueString_m = "40";
-volatile uint32_t x=0;
-
+/*************************** Loop Code ************************************/
 void loop() {
   // Ensure the connection to the MQTT server is alive (this will make the first
   // connection and automatically reconnect when disconnected).  See the MQTT_connect
   // function definition further below.
+  static bool IsUpdated_b = false;
+  static bool IsON_b = false;
+  static bool firsttime = true;
   MQTT_connect();
   timeClient.update();
 
+  if (firsttime){
+    Publish_Log();
+    firsttime = false;
+  }
+
   // this is our 'wait for incoming subscription packets' busy subloop
   // try to spend your time here
-
   Adafruit_MQTT_Subscribe *subscription;
   while ((subscription = mqtt.readSubscription(5000))) {
     if (subscription == &onoffbutton) {
       Serial.print(F("Automation Mode(0:ON, 1:OFF): "));
       Serial.println((char *)onoffbutton.lastread);
+      ind += cx;
+      cx = snprintf ( buffer + ind, 5000-ind, "Automation Mode(0:ON, 1:OFF): %s\n", (char *)onoffbutton.lastread);
 
       if (atoi((char *)onoffbutton.lastread) == 0)
       {
@@ -145,31 +172,38 @@ void loop() {
       Serial.print(F("Hour Start: "));
       AdjStartValueString_h = String((char *)adjuststartvalue_h.lastread);
       Serial.println(AdjStartValueString_h);
+      ind += cx;
+      cx = snprintf ( buffer + ind, 5000-ind, "Hour Start: %u\n", *adjuststartvalue_h.lastread);
     }
     if (subscription == &adjuststartvalue_m)
     {
       Serial.print(F("Min Start: "));
       AdjStartValueString_m = String((char *)adjuststartvalue_m.lastread);
       Serial.println(AdjStartValueString_m);
+      ind += cx;
+      cx = snprintf ( buffer + ind, 5000-ind, "Min Start: %u\n", *adjuststartvalue_m.lastread);
     }
     if (subscription == &adjuststopvalue_h)
     {
-      Serial.print(F("Hour Start: "));
+      Serial.print(F("Hour Stop: "));
       AdjStopValueString_h = String((char *)adjuststopvalue_h.lastread);
       Serial.println(AdjStopValueString_h);
+      ind += cx;
+      cx = snprintf ( buffer + ind, 5000-ind, "Hour Stop: %u\n", *adjuststopvalue_h.lastread);
     }
     if (subscription == &adjuststopvalue_m)
     {
-      Serial.print(F("Hour Start: "));
+      Serial.print(F("Min Stop: "));
       AdjStopValueString_m = String((char *)adjuststopvalue_m.lastread);
       Serial.println(AdjStopValueString_m);
+      ind += cx;
+      cx = snprintf ( buffer + ind, 5000-ind, "Min Stop: %u\n", *adjuststopvalue_m.lastread);
     }
 
     if (subscription == &onoffbutton_man)
     {
       Serial.print(F("Manual value: "));
       Serial.println((char *)onoffbutton_man.lastread);
-
       if (atoi((char *)onoffbutton_man.lastread) == 0)
       {
         Manual_Set = 1;
@@ -179,31 +213,74 @@ void loop() {
         Manual_Set = 0;
       }
     }
+
+    // Set the value to true
+    IsUpdated_b = true;
   }
 
 
   // Process auto and manual mode
-  if ( Automate_Flag)
-  {
-    // Mode: Auto
-    Serial.println("Mode: Auto");
-    Serial.print("Current Time: ");
-    Serial.print(timeClient.getHours());
-    Serial.print(":");
-    Serial.print(timeClient.getMinutes());
-    Serial.print(":");
-    Serial.println(timeClient.getSeconds());
-    Serial.print("Setting of Start Time: ");
-    Serial.print((int)AdjStartValueString_h.toInt());
-    Serial.print(":");
-    Serial.println((int)AdjStartValueString_m.toInt());
-    Serial.print("Setting of Stop Time: ");
-    Serial.print((int)AdjStopValueString_h.toInt());
-    Serial.print(":");
-    Serial.println((int)AdjStopValueString_m.toInt());
-    if ((int)timeClient.getHours() == (int)AdjStartValueString_h.toInt())
+  if (IsUpdated_b) {
+    IsUpdated_b = false;
+
+    if ( Automate_Flag)
     {
-      if ((int)timeClient.getMinutes() == (int)AdjStartValueString_m.toInt())
+      // Mode: Auto
+      Serial.println("Mode: Auto");
+      Serial.print("Current Time: ");
+      Serial.print(timeClient.getHours());
+      Serial.print(":");
+      Serial.print(timeClient.getMinutes());
+      Serial.print(":");
+      Serial.println(timeClient.getSeconds());
+      ind += cx;
+      cx = snprintf ( buffer + ind, 5000-ind, "Mode: Auto\nCurrent Time: %d:%d:%d\n", timeClient.getHours(), timeClient.getMinutes(),timeClient.getSeconds());
+      Serial.print("Setting of Start Time: ");
+      Serial.print((int)AdjStartValueString_h.toInt());
+      Serial.print(":");
+      Serial.println((int)AdjStartValueString_m.toInt());
+      Serial.print("Setting of Stop Time: ");
+      Serial.print((int)AdjStopValueString_h.toInt());
+      Serial.print(":");
+      Serial.println((int)AdjStopValueString_m.toInt());
+      ind += cx;
+      cx = snprintf ( buffer + ind, 5000-ind, "Setting of Start Time: %d:%d\nSetting of Stop Time: %d:%d\n", (int)AdjStartValueString_h.toInt(),
+                    (int)AdjStartValueString_m.toInt(), (int)AdjStopValueString_h.toInt(), (int)AdjStopValueString_m.toInt());
+    }
+    else if (!Automate_Flag)
+    {
+      // Mode: Manual
+      Serial.println("Mode: Manual");
+      if (Manual_Set)
+      {
+        x = 1;
+        Publish_Value_x(x);
+        Serial.println("Motor & VAN: ON");
+        ind += cx;
+        cx = snprintf ( buffer + ind, 5000-ind, "Motor & VAN: ON");
+        digitalWrite(0,LOW);
+        delay(300);
+        digitalWrite(2,LOW);
+      }
+      else if (!Manual_Set)
+      {
+        x = 0;
+        Publish_Value_x(x);
+        Serial.println("Motor & VAN: OFF");
+        ind += cx;
+        cx = snprintf ( buffer + ind, 5000-ind, "Motor & VAN: OFF");
+        digitalWrite(0,HIGH);
+        digitalWrite(2,HIGH);
+      }
+    }
+    Publish_Log();
+  }
+
+  if (!IsON_b){
+    if ((int)timeClient.getHours() >= (int)AdjStartValueString_h.toInt())
+    {
+      if (((int)timeClient.getMinutes() >= (int)AdjStartValueString_m.toInt()) &&
+          ((int)timeClient.getMinutes() < (int)AdjStopValueString_m.toInt()))
       {
         x = 1;
         Publish_Value_x(x);
@@ -211,50 +288,31 @@ void loop() {
         digitalWrite(0,LOW);
         delay(300);
         digitalWrite(2,LOW);
-      }
-    }
-    else if ((int)timeClient.getHours() >= (int)AdjStopValueString_h.toInt())
-    {
-      if ((int)timeClient.getMinutes() >= (int)AdjStopValueString_m.toInt())
-      {
-        x = 0;
-        Publish_Value_x(x);
-        Serial.println("Motor & VAN: OFF");
-        digitalWrite(0,HIGH);
-        digitalWrite(2,HIGH);
+        IsON_b = true;
       }
     }
   }
-  else if (!Automate_Flag)
-  {
-    // Mode: Manual
-    Serial.println("Mode: Manual");
-    if (Manual_Set)
-    {
-      x = 1;
-      Publish_Value_x(x);
-      Serial.println("Motor & VAN: ON");
-      digitalWrite(0,LOW);
-      delay(300);
-      digitalWrite(2,LOW);
-    }
-    else if (!Manual_Set)
-    {
-      x = 0;
-      Publish_Value_x(x);
-      Serial.println("Motor & VAN: OFF");
-      digitalWrite(0,HIGH);
-      digitalWrite(2,HIGH);
-    }
+  else {
+    if ((int)timeClient.getHours() >= (int)AdjStopValueString_h.toInt())
+      {
+        if ((int)timeClient.getMinutes() >= (int)AdjStopValueString_m.toInt())
+        {
+          x = 0;
+          Publish_Value_x(x);
+          Serial.println("Motor & VAN: OFF");
+          digitalWrite(0,HIGH);
+          digitalWrite(2,HIGH);
+          IsON_b = false;
+        }
+      }
   }
 
   // ping the server to keep the mqtt connection alive
   // NOT required if you are publishing once every KEEPALIVE seconds
-  /*
   if(! mqtt.ping()) {
     mqtt.disconnect();
   }
-  */
+
 }
 
 void Publish_Value_x(uint32_t value)
@@ -268,6 +326,21 @@ void Publish_Value_x(uint32_t value)
   } else {
     Serial.println(F("OK!"));
   }
+}
+
+void Publish_Log(){
+  Serial.print(F("\nSending log to multiline text box "));
+  Serial.print("...");
+  if (! MultiTextReport.publish(buffer)) {
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("OK!"));
+  }
+  for (int i=0; i<ind; i++){
+    buffer[i] = 0;
+  }
+  cx = 0;
+  ind = 0;
 }
 
 // Function to connect and reconnect as necessary to the MQTT server.
